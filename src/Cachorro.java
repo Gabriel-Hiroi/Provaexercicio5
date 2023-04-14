aa8c62e0b2eab74fa1a441dc6b99afc6b1663efd
Gabriel-Hiroi <hiroigabriel@gmail.com>
2023-04-14 16:17:57 -0300

    Initial commit

---
+++ src/Cachorro.java
@@ -0,0 +1,6 @@
+public class Cachorro extends Animal {
+    @Override
+    public void emitirSom() {
+        System.out.println("au au au au au au au au au au Glauco é o maioral");
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Animal cachorro = new Cachorro();
+        cachorro.emitirSom();
+
+        Animal gato = new Gato();
+        gato.emitirSom();
+    }
+}
